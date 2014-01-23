65afafb43a890c09634a1e0e6e22d385b66f6d4b
David Price <david@blacktonic.com>
2014-01-22 21:57:39 -0800

    Chapter 11: Fixed error in publications.js

--- server/publications.js
+++ server/publications.js
@@ -7,5 +7,5 @@ Meteor.publish('comments', function(postId) {
 });
 
 Meteor.publish('notifications', function() {
-  return Notifications.find(userId: this.userId);
-})
+  return Notifications.find({userId: this.userId});
+});
