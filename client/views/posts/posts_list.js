7c2fc094632144afcd7119b31d66e045d2e85d7b
David Price <david@blacktonic.com>
2014-01-24 18:00:18 -0800

    BROKEN - Finished Chapter 12

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,6 @@
 Template.postsList.helpers({
-  posts: function() {
-    return Posts.find({}, {sort: {submitted: -1}});
+  hasMorePosts: function(){
+    this.posts.rewind();
+    return Router.current().limit() == this.posts.fetch().length;
   }
-})
+});
--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,11 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function(options) {
+  return Posts.find({}, options);
 });
+
+Meteor.publish('singlePost', function(id) {
+  return id && Posts.find(id);
+});
+
 
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
